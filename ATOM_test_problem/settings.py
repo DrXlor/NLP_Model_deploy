f8c73838af0b416e00d03d566b50587767211e52
DrXlor <eugeny@live.ru>
2022-02-03 18:05:09 +0300

    mn fix

--- ATOM_test_problem/settings.py
+++ ATOM_test_problem/settings.py
@@ -36,6 +36,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'predict/static'),
 )
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "predict"
+    "predict",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +60,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ATOM_test_problem.urls'
