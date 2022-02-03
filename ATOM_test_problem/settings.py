a6e9d7af8e02d161a81af612eccab3ec45e885b4
DrXlor <eugeny@live.ru>
2022-02-03 16:07:12 +0300

    dep fixed

--- ATOM_test_problem/settings.py
+++ ATOM_test_problem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iq3t5!osssl7^zkl5ks*^l%kw))9i@!gp!ciqiu+d3vkni7^13'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://imdbfilmreviewclassifier.herokuapp.com/', '*']
 
 
 # Application definition
