bb053019081e0a8da085d74d94bd91452b222275
DrXlor <eugeny@live.ru>
2022-02-03 18:22:29 +0300

    mn fix

--- ATOM_test_problem/settings.py
+++ ATOM_test_problem/settings.py
@@ -32,10 +32,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'predict/static'),
-)
-
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: gunicorn ATOM_test_problem.wsgi
